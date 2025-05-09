5f6aa9125252c8153f1951ee0599c86ebd30c2bd
phpontes <ppontes95@hotmail.com>
2025-05-09 12:31:28 -0300

    Refactor phase: improving the code quality

--- src/main/java/com/paulopontes/taskmanager/firsttddtest/Calculator.java
+++ src/main/java/com/paulopontes/taskmanager/firsttddtest/Calculator.java
@@ -3,7 +3,6 @@ package com.paulopontes.taskmanager.firsttddtest;
 public class Calculator {
 
     public double divide(double a, double b) {
-        double result = a / b;
-        return result;
+        return a / b;
     }
 }
