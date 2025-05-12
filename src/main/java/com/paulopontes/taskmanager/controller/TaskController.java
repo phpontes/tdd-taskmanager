b351a50b5d07e9118b5ed1eeee168d13b5852882
phpontes <ppontes95@hotmail.com>
2025-05-12 08:59:01 -0300

    Implementing the method of the controller

--- src/main/java/com/paulopontes/taskmanager/controller/TaskController.java
+++ src/main/java/com/paulopontes/taskmanager/controller/TaskController.java
@@ -1,7 +1,22 @@
 package com.paulopontes.taskmanager.controller;
 
+import com.paulopontes.taskmanager.model.Task;
+import com.paulopontes.taskmanager.service.TaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/tasks")
 public class TaskController {
+    @Autowired
+    private TaskService taskService;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
 }
